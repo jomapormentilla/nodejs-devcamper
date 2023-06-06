8d9b6a93c8d78b1c282e49feba3050ce2fdf8d0b
jomapormentilla <jomapormentilla@gmail.com>
2023-06-06 17:51:37 -0400

    Controllers and Routes setup

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+// Import route files
+const bootcamps = require('./routes/bootcamps')
+
 // Load ENV vars
 dotenv.config({ path: './config/config.env' })
-const app = express()
-const PORT = process.env.PORT || 5000
 
+// Create express app
+const app = express()
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+// Run express server
+const PORT = process.env.PORT || 5000
 app.listen(
     PORT, 
     console.log(`Server running in ${ process.env.NODE_ENV } mode on port ${ PORT }`)
